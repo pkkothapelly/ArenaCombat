70f5c15f7feb1b937bef2eac5e6a7ed588152dca
Pavan Kumar Kothapelly <pkkothapelly@gmail.com>
2026-09-11 09:59:21 +0100

    Add combat loop and death checks

--- ArenaCombat.cpp
+++ ArenaCombat.cpp
@@ -11,21 +11,36 @@ int main()
 	int playerHealth = 100;
 	int enemyHealth = 100;
 	int playerDamage = 10;
-	int enemyDamage = 10;
+	int enemyDamage = 20;
+	while (playerHealth>0 && enemyHealth>0) {
+		cout << "Player Health " << playerHealth << '\n';
+		cout << "Enemy Health " << enemyHealth << "\n\n";
 
-	cout << "Player Health " << playerHealth << '\n';
-	cout << "Enemy Health " << enemyHealth << "\n\n";
+		enemyHealth = enemyHealth - playerDamage;
 
-	enemyHealth = enemyHealth - playerDamage;
+		cout << "Player attacks for " << playerDamage << "\n";
+		cout << "Enemy Health " << enemyHealth << "\n\n";
 
-	cout << "Player attacks for " << playerDamage << "\n";
-	cout << "Enemy Health " << enemyHealth << "\n\n";
+		if (enemyHealth<=0) {
+			cout << "Enemy is Dead\n";
+			break;
+		}
+		
+		playerHealth = playerHealth - enemyDamage;
+		
+		
+	
+		cout << "Enemy attacks for " << enemyDamage << "\n";
+		cout << "Player Health " << playerHealth << "\n\n";
+		
+		if (playerHealth <= 0) {
+			cout << "Player is Dead\n";
+			break;
+		}
 
-	playerHealth = playerHealth - enemyDamage;
-
-	cout << "Enemy attacks for " << enemyDamage << "\n";
-	cout << "Player Health " << playerHealth << "\n";
-
+		cout << "	*****New Round*****		\n\n";
+	}
+	
 
 	return 0;
 }
