92f56348599b06804162e9b9fd7cde9be9e883cc
Pavan Kumar Kothapelly <pkkothapelly@gmail.com>
2026-09-23 09:50:55 +0100

    Refactor combat actions and add healing

--- ArenaCombat.cpp
+++ ArenaCombat.cpp
@@ -4,83 +4,129 @@
 #include <iostream>
 using namespace std;
 
+
+
+
+void showHealth(int pHealth, int eHealth) {
+	cout <<"Player Health: " << pHealth << '\n';
+	cout <<"Enemy Health: " << eHealth << "\n\n";
+}
+
+int getPlayerChoice() {
+
+	int choice; 
+
+	
+	cin >> choice;
+	while (choice != 1 && choice != 2 && choice !=3) {
+			cout << "\nDont be blind, look at the choices again twat\n";
+			cout << "Choose Action: ";
+			cin >> choice;
+	}
+
+	return choice;
+}
+
+bool isDead(int health) {
+
+	return health <= 0;
+
+}
+
+void playerAttack(int& eHealth, int pDamage) {
+
+	eHealth = eHealth - pDamage ;
+
+}
+
+void enemyAttack(int& pHealth, int enemyDamage, bool isPDefending) {
+
+	if (isPDefending) {
+		pHealth = pHealth - enemyDamage/2;
+		
+	}
+	else {
+		pHealth = pHealth - enemyDamage;
+	}
+
+
+}
+
+void healPlayer(int& pHealth, int healAmount) {
+	
+		pHealth = pHealth + healAmount;
+	
+		if (pHealth>100) {
+			pHealth = 100;
+		}
+}
+
+
 int main()
 {
-	cout << "	==ARENA COMBAT==	 \n\n";
-
 	int playerHealth = 100;
 	int enemyHealth = 100;
 	int playerDamage = 10;
-	int enemyDamage = 20;
+	int enemyDamage = 10;
 	int choice;
+
+
+	cout << "	==ARENA COMBAT==	 \n\n";
+
 	bool isPlayerDefending = false;
 	while (playerHealth>0 && enemyHealth>0) {
 
 		
-		cout << "Player Health " << playerHealth << '\n';
-		cout << "Enemy Health " << enemyHealth << "\n\n";
+		
 
 		cout << "1. Attack\n";
 		cout << "2. Defend\n";
+		cout << "3. Heal Yourself\n";
 		
-		cin >> choice;
-		while (choice !=1 && choice!=2) {
-			cout << "\nDont be blind, look at the choices again twat\n";
-			cout << "Choose Action: ";
-			cin >> choice;
+		
+		
+			
+		choice = getPlayerChoice();
 
-		}
+		
 		
 
 		if (choice == 1) {
 			isPlayerDefending = false;
-			enemyHealth = enemyHealth - playerDamage;
-
-			cout << "\nPlayer attacks for " << playerDamage << "\n";
-			cout << "Enemy Health " << enemyHealth << "\n\n";
+			playerAttack(enemyHealth, playerDamage);
 
 		}
+
 		
 
 		else if (choice == 2) {
-			isPlayerDefending = true;
 			
-				/*playerHealth = playerHealth - enemyDamage;
+			isPlayerDefending = true;
 
-
-
-				cout << "Enemy attacks for " << enemyDamage << "\n";
-				cout << "Player Health " << playerHealth << "\n\n";*/
-
-				cout << "\nPlayer Defends\n";
-				
 			}
 
-		if (enemyHealth <= 0) {
-			cout << "Enemy is Dead\n";
+		else if (choice == 3) {
+			isPlayerDefending = false;
+			healPlayer(playerHealth, 15);
+
+		}
+
+		if (isDead(enemyHealth))
+		{
+			cout << "\nEnemy is Dead\n";
 			break;
 		}
 
-		if (isPlayerDefending) {
-			playerHealth = playerHealth - enemyDamage / 2;
-			cout << "\nPlayer Health is " << playerHealth << "\n";
-			cout << "Enemy Health is " << enemyHealth << "\n";
-		}
-
-			else {
-				playerHealth = playerHealth - enemyDamage;
-
-				cout << "Enemy Attacks with "<<enemyDamage<<"\n";
-				cout << "\nPlayer Health is " << playerHealth << "\n";
-			}
-		
+		enemyAttack(playerHealth, enemyDamage, isPlayerDefending);
 
 		
 
-		if (playerHealth <= 0) {
+		if (isDead(playerHealth)) {
 			cout << "Player is Dead\n";
 			break;
 		}
+
+		showHealth(playerHealth,enemyHealth);
 
 		
 
